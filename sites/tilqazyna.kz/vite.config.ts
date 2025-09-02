1157e23e571efea3788b6d315e13044886c2d987
Saken Tsukenofu <110395849+sakentsunofu@users.noreply.github.com>
2025-09-02 14:37:31 +0500

    Add TypeScript effective configuration and update Vite config for path aliasing

--- sites/tilqazyna.kz/vite.config.ts
+++ sites/tilqazyna.kz/vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig(({ command }) => ({
     base: command === 'build' ? './' : undefined,
@@ -18,6 +19,11 @@ export default defineConfig(({ command }) => ({
             formVariants: true,
         }),
     ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'resources/js'),
+        },
+    },
     esbuild: {
         jsx: 'automatic',
     },
