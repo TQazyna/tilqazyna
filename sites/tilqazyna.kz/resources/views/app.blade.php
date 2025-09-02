1e749217226eb437e215f9b9b7111cc91163b4b9
Saken Tsukenofu <110395849+sakentsunofu@users.noreply.github.com>
2025-09-02 14:31:00 +0500

    Remove commented-out dark mode script from app.blade.php to clean up the code and improve readability.

--- sites/tilqazyna.kz/resources/views/app.blade.php
+++ sites/tilqazyna.kz/resources/views/app.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         {{-- Inline script to detect system dark mode preference and apply it immediately --}}
+        {{-- 
         <script>
             (function() {
                 const appearance = '{{ $appearance ?? "system" }}';
@@ -18,6 +19,7 @@
                 }
             })();
         </script>
+        --}}
 
         {{-- Inline style to set the HTML background color based on our theme in app.css --}}
         <style>
